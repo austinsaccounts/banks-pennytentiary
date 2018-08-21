a913312469606655e8a3695b8160d90be1360ce6
austin cummings and Kendra Kelly <austin@>
2018-08-21 16:23:11 -0700

    track account names and balances

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,44 @@
 // business logic
-var name2="jesus";
 var customers = [];
+var custName;
 var customer = function(name, deposit) {
   this.name = name;
   this.balance = deposit;
 };
 
-customer.prototype.withDrawal = function(customer, wdAmount) {
+customer.prototype.withDrawal = function(customer, wdAmount) {};
 
-};
 customer.prototype.deposit = function(customer, depAmount) {
 
 };
 
-    var customerSearch = function(){
-      for (var i=0; i<customers.length;i++) {
-    if (customers[i].name === name2) {
-        return customers[i].balance;
-    } else if (customers[i].name === name2) && ("input#withdrawlAmount") {
-        return customer[i] - lessCash;
-    } else if (customers[i].name === name2) && ("input#depositAmount") {
-        return customer[i] + newCash;
-    } else {
-      alert("try again stupid");
+var customerSearch = function() {
+  for (var i = 0; i < customers.length; i++) {
+    if (customers[i].name === custName) {
+      return parseInt(customers[i].balance);
     }
   }
-}
-  // };
+};
 
 // interface logic
 $(document).ready(function() {
   $("#newAccountform").submit(function(event) {
     event.preventDefault();
-    console.log("the submit works");
     var name = $("#customerName").val();
-    var deposit = $("#initialDeposit").val();
+    var deposit = parseInt($("#initialDeposit").val());
     var newCustomer = new customer(name, deposit);
-    var lessCash = $("#withdrawlAmount").val();
-    var newCash = $("#depositAmount").val();
     customers.push(newCustomer)
-
     $("#showBalance").text(newCustomer.balance);
-// console.log(customerSearch());
+  })
+  $("#depositWithdrawlform").submit(function(event) {
+    event.preventDefault();
+    custName = $("#customerName").val();
+    var lessCash = parseFloat($("#withdrawlAmount").val());
+    var newCash = parseFloat($("#depositAmount").val());
+    var oldTotal = customerSearch();
+    var newTotal = parseFloat(oldTotal + newCash - lessCash);
+    alert(newTotal);
 
-  // $("#depositWithdrawlform").submit(function(event) {
-  //   event.preventDefault();
-    // var newDeposit = $("#depositAmount").val();
-    // var newWithdrawl = $("#withdrawlAmount").val();
-  // });
-
-
-
-
-});
+    $("#showBalance").text("Hello" + custName + "You're new balance is..." + newTotal);
+  })
 });
